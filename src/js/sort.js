7ec006fe0455ba7df7c2c39dc19c0109003db5f8
Robert Laws <roblaws@me.com>
2019-03-12 13:02:58 -0400

    enhance sortable interface

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+  var sortedIDs = [];
   $("#select-zone, #drop-zone").sortable({
     connectWith: ".connect-list",
+    cursor: "pointer",
+    handle: ".sort-handle",
+    placeholder: "ui-state-highlight",
     dropOnEmpty: true
-
   }).disableSelection();
+
+  $("#get-items").click(function() {
+    var sortedIDs = $( "#drop-zone" ).sortable( "toArray" );
+    alert(sortedIDs);
+  });
 });
+
+
+
