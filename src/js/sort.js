7b970afe4e73f98ed8b8a9a066bc10567bc69809
Robert Laws <roblaws@me.com>
2019-03-21 14:52:25 +0300

    add json data and getJSON loading

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   var sortedIDs = [];
 
+
+  var instructionItem = $("<li id='item-6'><span class='sort-handle'>Sample Item</span><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis assumenda voluptates!</p><p>10</p><p><a id='item6' href='#'>More Information</a></p></li><div id='dialog6' title='Item # 6 Details'><p>Here are more details about Item # 6.</p></div>");
+
+  
+
+  $("ul#select-zone").append(instructionItem);
+
+  $("#dialog6").dialog({
+    autoOpen: false
+  });
+
+  $("#item6").click(function(event) {
+    event.preventDefault();
+    $("#dialog6").dialog("open");
+  });
+
   $("#select-zone, #drop-zone").sortable({
     connectWith: ".connect-list",
     cursor: "pointer",
@@ -11,6 +27,51 @@ $(document).ready(function() {
 
   $("#get-items").click(function() {
     var sortedIDs = $("#drop-zone").sortable("toArray");
-    alert(sortedIDs);
+    console.log(sortedIDs);
+    if(sortedIDs.length != 0) {
+      alert(sortedIDs);
+    } else {
+      alert('no selections')
+    }
   });
 });
+
+$.getJSON("assets/data/instruction.json")
+  .done(function(items) {
+    var ajaxArea = $("section#ajax-space");
+    var itemNumber = 7;
+    console.log(items.instruction);
+    items.instruction.forEach(function(unit) {
+      var id = unit.id;
+      var title = unit.title;
+      var description = unit.description;
+      var time = unit.default_time;
+      var outcomes = "";
+      var assessment = "";
+      unit.learning_outcomes.forEach(function(outcome) {
+        outcomes += "<li>" + outcome + "</li>"
+      });
+      unit.activities.forEach(function(activity) {
+        assessment += "<li>" + activity + "</li>"
+      });
+      var modal = "<p><strong>Brief Description:</strong> " + description + "</p><p><strong>Learning Outcomes</strong> (students will be able to:)<br><ul>" + outcomes + "</ul></p><p><strong>Activities/Assessment</strong><br><ul>" + assessment + "</ul></p>"
+      var dragItem = $("<li id='" + id + "'><span class='sort-handle'>" + title + "</span><p>" + description + "</p><p>" + time + "</p><p><a id='item" + id + "' href='#'>More Information</a></p></li><div id='dialog" + id + "' title='" + title + "'><p>" + modal + "</p></div>");
+
+      // ajaxArea.append(dragItem);
+      $("ul#select-zone").append(dragItem);
+
+      $("#dialog" + id).dialog({
+        autoOpen: false,
+        modal: true,
+        minHeight: 400,
+        minWidth: 800
+      });
+    
+      $("#item" + id).click(function(event) {
+        event.preventDefault();
+        $("#dialog" + id).dialog("open");
+      });
+
+      itemNumber++;
+    });
+  });
